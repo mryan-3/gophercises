7698d8b494ccec12c8b5a3ce8a62f871c2a02754
mryan-3 <malawaryan@gmail.com>
2024-01-05 11:32:07 +0300

    added a function that parses the records and returns them as a slice of a question and an answer

--- QuizGame/main.go
+++ QuizGame/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
-    "encoding/csv"
-    "fmt"
-    "log"
-    "os"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"log"
+	"os"
 )
 
 func main (){
-    file, err := os.Open("./problems.csv")
+    csvFile := flag.String("csv", "problems.csv", "A csv file in the format question, answer")
+    flag.Parse()
+    file, err := os.Open(*csvFile)
 
     if err != nil {
-        log.Fatal("Error while opening the file")
+        log.Fatal("Error while opening the file ", *csvFile)
     }
 
     defer file.Close()
@@ -23,8 +26,22 @@ func main (){
     if err != nil{
         fmt.Println("Error while reading the records")
     }
+    parseRecords(records)
+}
 
-    for _, eachRecord := range records{
-        fmt.Println(eachRecord)
+func parseRecords(records [][]string) []problem{
+    ret := make([]problem, len(records))
+    for i, record := range records{
+        ret[i] = problem{
+            q: record[0],
+            a: record[1],
+        }
     }
+    fmt.Println(ret)
+    return ret
+}
+
+type problem struct{
+    q string
+    a string
 }
