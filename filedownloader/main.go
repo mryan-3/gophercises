3616b8f26e3524e5c4c46ed9950aa7bcbf17b819
mryan-3 <malawaryan@gmail.com>
2024-01-17 21:31:50 +0300

    Built the downloader that works

--- filedownloader/main.go
+++ filedownloader/main.go
@@ -1,29 +1,58 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "log"
-    "net/http"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
 )
 
-
-func main(){
-    fmt.Println("THello from the moon")
-
-    res, err := http.Get("http://www.google.com/robots.txt")
+func downloadFile(fileName string, urlPath string)(err error){
+    //Build filename from the url path
+    fileUrl, err := url.Parse(urlPath)
     if err != nil {
         log.Fatal(err)
     }
-    body, err := io.ReadAll(res.Body)
-    res.Body.Close()
+    path := fileUrl.Path
+    segments := strings.Split(path, "/")
+    fileName = segments[len(segments)-1]
 
-    if res.StatusCode > 299 {
-        log.Fatal("Response failed with status code %d and \nbody: %s\n", res.StatusCode, body)
+    //Create the downloadFile
+    file, err := os.Create(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    //Get the data
+    resp, err := http.Get(urlPath)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer resp.Body.Close()
+
+    //Check the status
+    if resp.StatusCode != http.StatusOK{
+        log.Fatalf("Response failed with status code: %d and\nbody: %s\n", resp.StatusCode, resp.Body)
     }
 
+    //Write the body to the file
+    size, err := io.Copy(file, resp.Body)
     if err != nil{
         log.Fatal(err)
     }
-    fmt.Printf("%s", body)
+    defer file.Close()
+    fmt.Printf("Downloaded a file %s with size %d", fileName, size)
+
+    return nil
+}
+
+
+func main(){
+   fmt.Println("THello from the moon")
+    downloadFile("robots", "http://www.google.com/robots.txt")
+
 }
